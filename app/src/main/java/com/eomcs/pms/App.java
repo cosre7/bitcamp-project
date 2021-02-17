4fd4eb30f9f8fd1c58d52a1bf3df40fd3c56cb0e
cosre7 <cosre7@gmail.com>
2021-02-17 11:38:44 +0900

    23-1

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -31,84 +31,90 @@ public class App {
 
         // 사용자가 입력한 명령을 보관해둔다.
         commandStack.push(command);
-        // add 대신 push를 쓰는 이유?
-        // stack 에서 add는 push로 표현되기 때문에 직관성을 위해서!
-
         commandQueue.offer(command);
 
-        switch (command) {
-          case "/member/add":
-            memberHandler.add();
-            break;
-          case "/member/list":
-            memberHandler.list();
-            break;
-          case "/member/detail":
-            memberHandler.detail();
-            break;  
-          case "/member/update":
-            memberHandler.update();
-            break; 
-          case "/member/delete":
-            memberHandler.delete();
-            break;
-          case "/project/add":
-            projectHandler.add();
-            break;
-          case "/project/list":
-            projectHandler.list();
-            break;
-          case "/project/detail": 
-            projectHandler.detail();
-            break;  
-          case "/project/update":
-            projectHandler.update();
-            break; 
-          case "/project/delete":
-            projectHandler.delete();
-            break;
-          case "/task/add":
-            taskHandler.add();
-            break;
-          case "/task/list":
-            taskHandler.list();
-            break;
-          case "/task/detail": 
-            taskHandler.detail();
-            break;  
-          case "/task/update":
-            taskHandler.update();
-            break; 
-          case "/task/delete":
-            taskHandler.delete();
-            break;
-          case "/board/add":
-            boardHandler.add();
-            break;
-          case "/board/list":
-            boardHandler.list();
-            break;
-          case "/board/detail":
-            boardHandler.detail();
-            break;  
-          case "/board/update":
-            boardHandler.update();
-            break; 
-          case "/board/delete":
-            boardHandler.delete();
-            break;
-          case "history": // <== history 명령 추가
-            printCommandHistory(commandStack.iterator());
-            break;
-          case "history2":
-            printCommandHistory(commandQueue.iterator());
-            break;
-          case "quit":
-          case "exit":
-            System.out.println("안녕!");
-            break loop;
-          default:
-            System.out.println("실행할 수 없는 명령입니다.");
+        try {
+          switch (command) {
+            case "/member/add":
+              memberHandler.add();
+              break;
+            case "/member/list":
+              memberHandler.list();
+              break;
+            case "/member/detail":
+              memberHandler.detail();
+              break;  
+            case "/member/update":
+              memberHandler.update();
+              break; 
+            case "/member/delete":
+              memberHandler.delete();
+              break;
+            case "/project/add":
+              projectHandler.add();
+              break;
+            case "/project/list":
+              projectHandler.list();
+              break;
+            case "/project/detail": 
+              projectHandler.detail();
+              break;  
+            case "/project/update":
+              projectHandler.update();
+              break; 
+            case "/project/delete":
+              projectHandler.delete();
+              break;
+            case "/task/add":
+              taskHandler.add();
+              break;
+            case "/task/list":
+              taskHandler.list();
+              break;
+            case "/task/detail": 
+              taskHandler.detail();
+              break;  
+            case "/task/update":
+              taskHandler.update();
+              break; 
+            case "/task/delete":
+              taskHandler.delete();
+              break;
+            case "/board/add":
+              boardHandler.add();
+              break;
+            case "/board/list":
+              boardHandler.list();
+              break;
+            case "/board/detail":
+              boardHandler.detail();
+              break;  
+            case "/board/update":
+              boardHandler.update();
+              break; 
+            case "/board/delete":
+              boardHandler.delete();
+              break;
+            case "history": // <== history 명령 추가
+              printCommandHistory(commandStack.iterator());
+              break;
+            case "history2":
+              printCommandHistory(commandQueue.iterator());
+              break;
+            case "quit":
+            case "exit":
+              System.out.println("안녕!");
+              break loop;
+            default:
+              System.out.println("실행할 수 없는 명령입니다.");
+          }
+        } catch (Exception e) {
+          System.out.println("----------------------------------------");                                                       
+          System.out.printf("명령어 실행 중 오류 발생: %s\n", e.getMessage());
+          // 예외 메시지 출력
+          // 예외 처리 -> 예외가 발생했을 때 프로그램을 멈추지 않고
+          // 오류 메시지만 출력하고 프로그램은 계속해서 작동되도록 하기 위함
+          System.out.println("----------------------------------------");
         }
         System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
       }
